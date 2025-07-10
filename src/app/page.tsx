e89abc4088f44422ce5c184347c2414d579fbaa3
AryanVerma01 <itzaryanverma@gmail.com>
2025-07-10 14:42:15 +0000

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { prisma } from "@/lib/db";
+import Image from "next/image";
+
+export default async function Home() {
+
+  const users = await prisma.user.findMany();
+
+  return (
+    <div> 
+      {
+        JSON.stringify(users)
+      }
+    </div>
+  );
+}
